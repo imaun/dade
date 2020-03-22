cff3b132702c46e41e71b9eed7b3313b0da96f29
iman nemati <imun22@gmmail.com>
2020-03-23 02:31:03 +0430

    Added support for .NET standard

---
+++ src/imun.Dade.Core/UnitOfWorkFactory.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Data;
+
+namespace imun.Dade.Core
+{
+    public interface IUnitOfWorkFactory
+    {
+        IUnitOfWork Create();
+    }
+
+    public class UnitOfWorkFactory<TConnection> : IUnitOfWorkFactory where TConnection : IDbConnection, new()
+    {
+        private string connectionString;
+
+        public UnitOfWorkFactory(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentNullException("connectionString cannot be null");
+            }
+
+            this.connectionString = connectionString;
+        }
+
+        public IUnitOfWork Create()
+        {
+            return new UnitOfWork(CreateConnection());
+        }
+
+        private IDbConnection CreateConnection()
+        {
+            var connection = new TConnection { ConnectionString = connectionString };
+            return connection;
+        }
+    }
+}
